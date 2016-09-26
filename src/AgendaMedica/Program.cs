a6b121050cb3210c73fe34455ca90ad5f0dfff4d
Carolina Castellani <carolinacastellani@gmail.com>
2016-09-26 15:26:02 -0300

    Criação do MDI Parent - Cadastrar já abre dentro da janela.

--- src/AgendaMedica/Program.cs
+++ src/AgendaMedica/Program.cs
@@ -13,7 +13,7 @@ namespace AgendaMedica {
         static void Main() {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main());
+            Application.Run(new MDI_AM());
         }
     }
 }
