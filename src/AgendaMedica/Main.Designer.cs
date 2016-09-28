5c42f6a75a74b69f1640f48a352e9340d0e79612
Raul <Raul@DESKTOP-BEP4IN3>
2016-09-28 20:26:20 -0300

    Refactoring na classe MDI Alteração no nome para MAIN; Refatoração nos nomes dos componentes internos; Abstração do método para abrir Tela; Limpeza de códigos e componentes obsoletos;

---
+++ src/AgendaMedica/Main.Designer.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace AgendaMedica
+{
+    partial class MDI_AM
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MDI_AM));
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.menuArquivo = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemCadastrar = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuItemPesquisar = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.menuItemSair = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuSobre = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStrip = new System.Windows.Forms.ToolStrip();
+            this.btnCadastrar = new System.Windows.Forms.ToolStripButton();
+            this.btnPesquisar = new System.Windows.Forms.ToolStripButton();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.menuStrip.SuspendLayout();
+            this.toolStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuArquivo,
+            this.menuSobre});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(870, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "MenuStrip";
+            // 
+            // menuArquivo
+            // 
+            this.menuArquivo.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menuItemCadastrar,
+            this.menuItemPesquisar,
+            this.toolStripSeparator3,
+            this.menuItemSair});
+            this.menuArquivo.ImageTransparentColor = System.Drawing.SystemColors.ActiveBorder;
+            this.menuArquivo.Name = "menuArquivo";
+            this.menuArquivo.Size = new System.Drawing.Size(61, 20);
+            this.menuArquivo.Text = "&Arquivo";
+            this.menuArquivo.Click += new System.EventHandler(this.menuArquivo_Click);
+            // 
+            // menuItemCadastrar
+            // 
+            this.menuItemCadastrar.Image = ((System.Drawing.Image)(resources.GetObject("menuItemCadastrar.Image")));
+            this.menuItemCadastrar.ImageTransparentColor = System.Drawing.Color.Black;
+            this.menuItemCadastrar.Name = "menuItemCadastrar";
+            this.menuItemCadastrar.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.N)));
+            this.menuItemCadastrar.Size = new System.Drawing.Size(167, 22);
+            this.menuItemCadastrar.Text = "&Cadastrar";
+            this.menuItemCadastrar.Click += new System.EventHandler(this.ShowNewForm);
+            // 
+            // menuItemPesquisar
+            // 
+            this.menuItemPesquisar.Image = ((System.Drawing.Image)(resources.GetObject("menuItemPesquisar.Image")));
+            this.menuItemPesquisar.ImageTransparentColor = System.Drawing.Color.Black;
+            this.menuItemPesquisar.Name = "menuItemPesquisar";
+            this.menuItemPesquisar.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.L)));
+            this.menuItemPesquisar.Size = new System.Drawing.Size(167, 22);
+            this.menuItemPesquisar.Text = "&Pesquisar";
+            this.menuItemPesquisar.Click += new System.EventHandler(this.menuItemPesquisar_Click);
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(164, 6);
+            // 
+            // menuItemSair
+            // 
+            this.menuItemSair.Image = ((System.Drawing.Image)(resources.GetObject("menuItemSair.Image")));
+            this.menuItemSair.Name = "menuItemSair";
+            this.menuItemSair.ShortcutKeys = ((System.Windows.Forms.Keys)((System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.W)));
+            this.menuItemSair.Size = new System.Drawing.Size(167, 22);
+            this.menuItemSair.Text = "S&air";
+            this.menuItemSair.Click += new System.EventHandler(this.ExitToolsStripMenuItem_Click);
+            // 
+            // menuSobre
+            // 
+            this.menuSobre.Name = "menuSobre";
+            this.menuSobre.Size = new System.Drawing.Size(49, 20);
+            this.menuSobre.Text = "Sobre";
+            this.menuSobre.Click += new System.EventHandler(this.sobreToolStripMenuItem_Click);
+            // 
+            // toolStrip
+            // 
+            this.toolStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnCadastrar,
+            this.btnPesquisar});
+            this.toolStrip.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip.Name = "toolStrip";
+            this.toolStrip.Size = new System.Drawing.Size(870, 25);
+            this.toolStrip.TabIndex = 1;
+            this.toolStrip.Text = "ToolStrip";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnCadastrar.Image = ((System.Drawing.Image)(resources.GetObject("btnCadastrar.Image")));
+            this.btnCadastrar.ImageTransparentColor = System.Drawing.Color.Black;
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(23, 22);
+            this.btnCadastrar.Text = "New";
+            this.btnCadastrar.ToolTipText = "Cadastrar";
+            this.btnCadastrar.Click += new System.EventHandler(this.ShowNewForm);
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnPesquisar.Image = ((System.Drawing.Image)(resources.GetObject("btnPesquisar.Image")));
+            this.btnPesquisar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.RightToLeftAutoMirrorImage = true;
+            this.btnPesquisar.Size = new System.Drawing.Size(23, 22);
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 504);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(870, 22);
+            this.statusStrip.TabIndex = 2;
+            this.statusStrip.Text = "StatusStrip";
+            // 
+            // toolStripStatusLabel
+            // 
+            this.toolStripStatusLabel.Name = "toolStripStatusLabel";
+            this.toolStripStatusLabel.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel.Text = "Status";
+            // 
+            // MDI_AM
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(870, 526);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.toolStrip);
+            this.Controls.Add(this.menuStrip);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MDI_AM";
+            this.Text = "Agenda Médica";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.toolStrip.ResumeLayout(false);
+            this.toolStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private void menuArquivo_Click(object sender, EventArgs e) {            
+        }
+        #endregion
+
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStrip toolStrip;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel;
+        private System.Windows.Forms.ToolStripMenuItem menuArquivo;
+        private System.Windows.Forms.ToolStripMenuItem menuItemCadastrar;
+        private System.Windows.Forms.ToolStripMenuItem menuItemSair;
+        private System.Windows.Forms.ToolStripButton btnCadastrar;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.ToolStripButton btnPesquisar;
+        private System.Windows.Forms.ToolStripMenuItem menuItemPesquisar;
+        private System.Windows.Forms.ToolStripMenuItem menuSobre;
+    }
+
+    
+}
+
+
+
---
+++ src/AgendaMedica/Main.cs
@@ -0,0 +1,63 @@
+﻿using AgendaMedica.Classes;
+using AgendaMedica.Database;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgendaMedica {
+    public partial class MDI_AM : Form {        
+
+        public MDI_AM() {
+            InitializeComponent();
+        }
+
+        private void ShowNewForm(object sender, EventArgs e) {
+            Cadastro frmCadastro = new Cadastro();
+            abreTela(frmCadastro);
+
+        }
+
+      
+
+        private void ExitToolsStripMenuItem_Click(object sender, EventArgs e) {
+            this.Close();
+        }
+
+      
+
+        private void CloseAllToolStripMenuItem_Click(object sender, EventArgs e) {
+            foreach (Form childForm in MdiChildren) {
+                childForm.Close();
+            }
+        }
+
+        private void menuItemPesquisar_Click(object sender, EventArgs e) {
+            Pesquisar formPesquisa = new Pesquisar();
+            abreTela(formPesquisa);
+        }
+
+        private void sobreToolStripMenuItem_Click(object sender, EventArgs e) {
+            Sobre formSobre = new Sobre();
+            abreTela(formSobre);
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e) {
+            Pesquisar formPesquisa = new Pesquisar();
+            abreTela(formPesquisa);
+        }
+
+
+
+        private void abreTela(Form tela) {
+            tela.MdiParent = this;
+            tela.Show();
+            tela.WindowState = FormWindowState.Maximized;
+        }
+    }
+}
