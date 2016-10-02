e938c3cef2f71ceb6733c9821b4e97b621f79d5e
Raul <Raul@DESKTOP-BEP4IN3>
2016-10-02 19:48:57 -0300

    Adicionada a classe propria de excessões

---
+++ src/AgendaMedica/Helpers/Exceptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgendaMedica.Helpers {
+    class Exceptions {
+        private Exception erro;
+        private string message;
+
+        public Exceptions(Exception e, string message) {
+            this.erro = e;
+            this.message = message;
+        }
+
+        public void lancaException() {
+            Console.WriteLine(this.erro);
+            MessageBox.Show(this.message);
+        }
+    }
+}
