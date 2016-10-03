dc324d69504b95a58c70d5b9f4951b4ecb73fcdf
Raul <Raul@DESKTOP-BEP4IN3>
2016-10-02 23:32:39 -0300

    Padrinização de class name

--- src/AgendaMedica/Helpers/ExceptionsHelper.cs
+++ src/AgendaMedica/Helpers/ExceptionsHelper.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AgendaMedica.Helpers {
-    class Exceptions {
+    class ExceptionsHelper {
         private Exception erro;
         private string message;
 
-        public Exceptions(Exception e, string message) {
+        public ExceptionsHelper(Exception e, string message) {
             this.erro = e;
             this.message = message;
         }
